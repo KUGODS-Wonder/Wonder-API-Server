45bb71885ae8de0b497a0b3632c4c088cf777079
great-park <cksgh1735@gmail.com>
2023-02-19 17:05:24 +0900

    feat: Spring DataJpa Configuration

---
+++ src/main/java/kugods/wonder/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package kugods.wonder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
