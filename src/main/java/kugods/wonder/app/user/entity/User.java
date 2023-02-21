ef571e0ed0a60780c81fde4afc53cc39835c1a19
great-park <cksgh1735@gmail.com>
2023-02-21 18:26:27 +0900

    feat: User entity static factory method

--- src/main/java/kugods/wonder/app/user/entity/User.java
+++ src/main/java/kugods/wonder/app/user/entity/User.java
@@ -2,9 +2,13 @@ package kugods.wonder.app.user.entity;
 
 import kugods.wonder.app.common.entity.BaseEntity;
 import kugods.wonder.app.record.entity.Tier;
+import lombok.Getter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
+@Getter
+@ToString
 @Table(name = "User")
 @Entity
 public class User extends BaseEntity {
@@ -33,4 +37,19 @@ public class User extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "tier_id")
     private Tier tier;
+
+    protected User() {}
+
+    protected User(String name, String address, String refreshToken, String email, String password, Tier tier){
+        this.name = name;
+        this.address = address;
+        this.refreshToken = refreshToken;
+        this.email = email;
+        this.password = password;
+        this.tier = tier;
+    }
+
+    public static User of(String name, String address, String refreshToken, String email, String password, Tier tier) {
+        return new User(name, address, refreshToken, email,password, tier);
+    }
 }
