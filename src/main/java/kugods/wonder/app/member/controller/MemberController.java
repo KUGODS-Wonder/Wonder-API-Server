44e63cd959fe793bbf05c544dff13915daa8eb6a
great-park <cksgh1735@gmail.com>
2023-02-22 15:53:41 +0900

    feat #1: controller for Member

--- src/main/java/kugods/wonder/app/member/controller/MemberController.java
+++ src/main/java/kugods/wonder/app/member/controller/MemberController.java
@@ -1,16 +1,37 @@
 package kugods.wonder.app.member.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import kugods.wonder.app.common.dto.ApiDataResponse;
+import kugods.wonder.app.member.dto.SigninRequest;
+import kugods.wonder.app.member.dto.SigninResponse;
+import kugods.wonder.app.member.dto.SignupRequest;
+import kugods.wonder.app.member.dto.SignupResponse;
+import kugods.wonder.app.member.repository.MemberRepository;
+import kugods.wonder.app.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class MemberController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello");
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    @PostMapping("/signin")
+    public ApiDataResponse<SigninResponse> signin(
+            @RequestBody SigninRequest request
+    ) {
+        return ApiDataResponse.of(memberService.signin(request));
+    }
+
+    @PostMapping("/signup")
+    public ApiDataResponse<SignupResponse> signup(
+            @RequestBody SignupRequest request
+    ) {
+        return ApiDataResponse.of(memberService.signup(request));
     }
 }
