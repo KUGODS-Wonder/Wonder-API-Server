eacfbb90068f4997c2eac7ce7840543b950cead7
great-park <cksgh1735@gmail.com>
2023-02-22 15:42:57 +0900

    feat #1: Custom UserDetails and its Service

---
+++ src/main/java/kugods/wonder/app/auth/domain/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package kugods.wonder.app.auth.domain;
+
+import kugods.wonder.app.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    public final Member getMember() {
+        return member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return member.getRoles().stream().map(o -> new SimpleGrantedAuthority(
+                o.getName()
+        )).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
