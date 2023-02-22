1eda2d6f2d376e763c16616a79116a6dd5741c87
great-park <cksgh1735@gmail.com>
2023-02-22 15:38:39 +0900

    feat #1: add Authority entity

---
+++ src/main/java/kugods/wonder/app/member/entity/Authority.java
@@ -0,0 +1,34 @@
+package kugods.wonder.app.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Column(name = "authority_id")
+    private Long authorityId;
+
+    private String name;
+
+    @JoinColumn(name = "member")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
--- src/main/java/kugods/wonder/app/member/entity/Member.java
+++ src/main/java/kugods/wonder/app/member/entity/Member.java
@@ -2,13 +2,17 @@ package kugods.wonder.app.member.entity;
 
 import kugods.wonder.app.common.entity.BaseEntity;
 import kugods.wonder.app.record.entity.Tier;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Member")
 @Entity
 public class Member extends BaseEntity {
@@ -18,17 +22,13 @@ public class Member extends BaseEntity {
     @Column(name = "member_id")
     private Long memberId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(name = "address", nullable = false)
     private String address;
 
-    @Column(name = "refresh_token", nullable = false)
-    private String refreshToken;
-
-
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
@@ -38,18 +38,12 @@ public class Member extends BaseEntity {
     @JoinColumn(name = "tier_id")
     private Tier tier;
 
-    protected Member() {}
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Authority> roles = new ArrayList<>();
 
-    protected Member(String name, String address, String refreshToken, String email, String password, Tier tier){
-        this.name = name;
-        this.address = address;
-        this.refreshToken = refreshToken;
-        this.email = email;
-        this.password = password;
-        this.tier = tier;
-    }
-
-    public static Member of(String name, String address, String refreshToken, String email, String password, Tier tier) {
-        return new Member(name, address, refreshToken, email,password, tier);
+    public void setRoles(List<Authority> role) {
+        this.roles = role;
+        role.forEach(o -> o.setMember(this));
     }
 }
