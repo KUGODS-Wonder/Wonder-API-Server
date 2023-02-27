1f15aaf2d7da7af4b7eaafc89bfb8453800ad0a8
great-park <cksgh1735@gmail.com>
2023-02-27 18:44:44 +0900

    fix #6: request url which is permitted

--- src/main/java/kugods/wonder/config/SecurityConfig.java
+++ src/main/java/kugods/wonder/config/SecurityConfig.java
@@ -61,12 +61,12 @@ public class SecurityConfig {
                 // 조건별로 요청 허용/제한 설정
                 .authorizeRequests()
                 // 회원가입과 로그인은 모두 승인
-                .antMatchers("/api/v1/signin", "/api/v1/signup", "/api/v1/hello").permitAll()
+                .antMatchers("/api/v1/members/signin", "/api/v1/members/signup", "/api/v1/members/google/login").permitAll()
                 .antMatchers("/login/getGoogleAuthUrl", "/login/oauth2/code/google").permitAll()
-                // /admin으로 시작하는 요청은 ADMIN 권한이 있는 유저에게만 허용
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                // /user 로 시작하는 요청은 USER 권한이 있는 유저에게만 허용
-                .antMatchers("/user/**").hasRole("USER")
+//                // /admin으로 시작하는 요청은 ADMIN 권한이 있는 유저에게만 허용
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+//                // /user 로 시작하는 요청은 USER 권한이 있는 유저에게만 허용
+//                .antMatchers("/user/**").hasRole("USER")
                 .anyRequest().denyAll()
                 .and()
                 // JWT 인증 필터 적용
