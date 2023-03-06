73e33b523caeaed3935068de77c665d8e124ca78
great-park <cksgh1735@gmail.com>
2023-03-06 23:49:34 +0900

    fix: BaseEntity - 필드 default 값 지정

--- src/main/java/kugods/wonder/app/common/entity/BaseEntity.java
+++ src/main/java/kugods/wonder/app/common/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package kugods.wonder.app.common.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
     private boolean isDeleted = false;
 
     @CreatedDate
