d6b0855e7677f11cc0270f6001e92d54b87b7bf0
great-park <cksgh1735@gmail.com>
2023-03-19 01:30:52 +0900

    refactor #17: add IntermediateLocation List and Tag List to WalkResponse

--- src/main/java/kugods/wonder/app/walk/service/WalkServiceImpl.java
+++ src/main/java/kugods/wonder/app/walk/service/WalkServiceImpl.java
@@ -4,16 +4,17 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import kugods.wonder.app.walk.dto.UserLocation;
-import kugods.wonder.app.walk.dto.WalkResponse;
+import kugods.wonder.app.walk.dto.*;
 import kugods.wonder.app.walk.exception.WalkDoesNotExistException;
 import kugods.wonder.app.walk.repository.WalkRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.querydsl.core.types.dsl.Expressions.numberTemplate;
 import static com.querydsl.core.types.dsl.MathExpressions.*;
@@ -24,6 +25,7 @@ import static kugods.wonder.app.walk.entity.QTag.tag;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WalkServiceImpl implements WalkService {
 
     private final JPAQueryFactory jpaQueryFactory;
@@ -34,8 +36,8 @@ public class WalkServiceImpl implements WalkService {
     @Override
     @Transactional(readOnly = true)
     public List<WalkResponse> getWalkList(UserLocation userLocation) {
-        return jpaQueryFactory
-                .select(Projections.constructor(WalkResponse.class,
+        List<WalkInfo> walks = jpaQueryFactory
+                .select(Projections.constructor(WalkInfo.class,
                         walk.walkId,
                         walk.title,
                         walk.distance,
@@ -46,18 +48,29 @@ public class WalkServiceImpl implements WalkService {
                         walk.destinationLatitude,
                         walk.destinationLongitude,
                         walk.point,
-                        calculateDistanceBetweenTwoPoint(userLocation).as("boundary"),
-                        intermediateLocation,
-                        tag
+                        calculateDistanceBetweenTwoPoint(userLocation).as("boundary")
                 ))
                 .from(walk)
-                .leftJoin(walk.intermediateLocations, intermediateLocation)
-                .leftJoin(walk.walkTagMatches, walkTagMatch)
-                .leftJoin(walkTagMatch.tag, tag)
-                .fetchJoin()
-                .distinct()
                 .orderBy(Expressions.numberPath(Double.class, "boundary").asc())
                 .fetch();
+
+        return walks.stream()
+                .map(walkInfo -> WalkResponse.builder()
+                        .walkId(walkInfo.getWalkId())
+                        .title(walkInfo.getTitle())
+                        .distance(walkInfo.getDistance())
+                        .requiredTime(walkInfo.getRequiredTime())
+                        .theme(walkInfo.getTheme())
+                        .originLatitude(walkInfo.getOriginLatitude())
+                        .originLongitude(walkInfo.getOriginLongitude())
+                        .destinationLatitude(walkInfo.getDestinationLatitude())
+                        .destinationLongitude(walkInfo.getDestinationLongitude())
+                        .point(walkInfo.getPoint())
+                        .boundary(walkInfo.getBoundary())
+                        .intermediateLocations(getIntermediateLocations(walkInfo.getWalkId()))
+                        .tagList(getTagList(walkInfo.getWalkId()))
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -102,6 +115,29 @@ public class WalkServiceImpl implements WalkService {
                                         .multiply(sin(radians(numberTemplate(BigDecimal.class, userLocation.getLatitude().toString())))))
                 ));
     }
+
+    private List<IntermediateLocationInfo> getIntermediateLocations(Long walkId) {
+        return jpaQueryFactory
+                .select(Projections.constructor(IntermediateLocationInfo.class,
+                        intermediateLocation.intermediateLocationId,
+                        intermediateLocation.latitude,
+                        intermediateLocation.longitude))
+                .from(intermediateLocation)
+                .where(intermediateLocation.walk.walkId.eq(walkId))
+                .fetch();
+    }
+
+    private List<TagInfo> getTagList(Long walkId) {
+        return jpaQueryFactory
+                .select(Projections.constructor(TagInfo.class,
+                        tag.tagId,
+                        tag.name))
+                .from(tag)
+                .join(walkTagMatch).on(tag.tagId.eq(walkTagMatch.tag.tagId))
+                .where(walkTagMatch.walk.walkId.eq(walkId))
+                .fetch();
+    }
+
 
     private void validateWalkExists(Long walkId) {
         if (walkRepository.findById(walkId).isEmpty()) {
