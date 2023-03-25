bfc193b37e80dbc85364017907c046c9eba53d22
great-park <cksgh1735@gmail.com>
2023-03-25 23:45:02 +0900

    feat #34: make Reservation API and cancel Reservation API

--- src/main/java/kugods/wonder/app/reservation/repository/ReservationRepository.java
+++ src/main/java/kugods/wonder/app/reservation/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package kugods.wonder.app.reservation.repository;
 import kugods.wonder.app.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Optional<Reservation> findOneByMember_EmailAndVoluntaryWork_VoluntaryWorkId(String email, Long voluntaryWorkId);
 }
