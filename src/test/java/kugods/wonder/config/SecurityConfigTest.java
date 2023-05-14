582df7a1fd152d8bec102e6a174b2ec99ab65be1
great-park <cksgh1735@gmail.com>
2023-05-14 22:32:50 +0900

    test: Security configuration test

---
+++ src/test/java/kugods/wonder/config/SecurityConfigTest.java
@@ -0,0 +1,46 @@
+package kugods.wonder.config;
+
+import kugods.wonder.app.auth.domain.jwt.TokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class SecurityConfigTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private TokenProvider tokenProvider;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void testPasswordEncodeBean() {
+        PasswordEncoder passwordEncoder = context.getBean(PasswordEncoder.class);
+        assertNotNull(passwordEncoder);
+    }
+}
