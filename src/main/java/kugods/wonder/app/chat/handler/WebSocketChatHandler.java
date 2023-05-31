0d12965c209bfb7ccf023626b8a196707b710881
great-park <cksgh1735@gmail.com>
2023-06-01 08:25:17 +0900

    feat #38: Web socket handler

---
+++ src/main/java/kugods/wonder/app/chat/handler/WebSocketChatHandler.java
@@ -0,0 +1,8 @@
+package kugods.wonder.app.chat.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+}
