3635d8882667e0f6a385ce2159b1a914abfc6d75
great-park <cksgh1735@gmail.com>
2023-06-01 17:13:51 +0900

    feat #44: Chat, ChatRoom Controller

--- src/main/java/kugods/wonder/app/chat/controller/ChatController.java
+++ src/main/java/kugods/wonder/app/chat/controller/ChatController.java
@@ -11,84 +11,54 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@Controller
 @RequiredArgsConstructor
+@Controller
 public class ChatController {
 
     private final SimpMessageSendingOperations template;
     private final ChatService chatService;
 
-
     @MessageMapping("/chat/enterUser")
     public void enterUser(@Payload ChatDto chat, SimpMessageHeaderAccessor headerAccessor) {
-        String roomId = chat.getRoomId();
-        chatService.plusUserCnt(roomId);
+        chatService.plusUserCnt(chat.getRoomId());
 
-        String userUUID = chatService.addUser(roomId, chat.getSender());
-        setSessionAttributes(headerAccessor, userUUID, roomId);
+        String userUUID = chatService.addUser(chat.getRoomId(), chat.getSender());
 
-        String message = generateMessage(chat.getSender(), " 님 입장!!");
-        chat.setMessage(message);
+        headerAccessor.getSessionAttributes().put("userUUID", userUUID);
+        headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
 
-        publishToChatRoom(roomId, chat);
+        chat.setMessage(chat.getSender() + " 님 입장!!");
+        template.convertAndSend("/sub/chatroom/detail/" + chat.getRoomId(), chat);
+
     }
 
     @MessageMapping("/chat/sendMessage")
     public void sendMessage(@Payload ChatDto chat) {
         log.info("CHAT {}", chat);
         chat.setMessage(chat.getMessage());
+        template.convertAndSend("/sub/chatroom/detail/" + chat.getRoomId(), chat);
 
-        publishToChatRoom(chat.getRoomId(), chat);
     }
 
     @EventListener
-    public void handleWebSocketDisconnect(SessionDisconnectEvent event) {
+    public void webSocketDisconnectListener(SessionDisconnectEvent event) {
         log.info("DisConnEvent {}", event);
 
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
         String userUUID = (String) headerAccessor.getSessionAttributes().get("userUUID");
         String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
 
         log.info("headAccessor {}", headerAccessor);
 
-        processUserDeparture(roomId, userUUID);
-    }
-
-    @GetMapping("/chat/userlist")
-    @ResponseBody
-    public List<String> userList(String roomId) {
-        return chatService.getUserList(roomId);
-    }
-
-    @GetMapping("/chat/duplicateName")
-    @ResponseBody
-    public String isDuplicateName(@RequestParam("roomId") String roomId, @RequestParam("username") String username) {
-        String userName = chatService.isDuplicateName(roomId, username);
-        log.info("동작확인 {}", userName);
-        return userName;
-    }
-
-    private void setSessionAttributes(SimpMessageHeaderAccessor headerAccessor, String userUUID, String roomId) {
-        headerAccessor.getSessionAttributes().put("userUUID", userUUID);
-        headerAccessor.getSessionAttributes().put("roomId", roomId);
-    }
-
-    private void publishToChatRoom(String roomId, ChatDto chat) {
-        template.convertAndSend("/sub/chat/room/" + roomId, chat);
-    }
-
-    private String generateMessage(String username, String action) {
-        return username + action;
-    }
-
-    private void processUserDeparture(String roomId, String userUUID) {
         chatService.minusUserCnt(roomId);
 
         String username = chatService.getUserName(roomId, userUUID);
@@ -96,13 +66,27 @@ public class ChatController {
 
         if (username != null) {
             log.info("User Disconnected : " + username);
+
             ChatDto chat = ChatDto.builder()
                     .type(ChatDto.MessageType.LEAVE)
                     .sender(username)
-                    .message(generateMessage(username, " 님 퇴장!!"))
+                    .message(username + " 님 퇴장!!")
                     .build();
 
-            publishToChatRoom(roomId, chat);
+            template.convertAndSend("/sub/chatroom/detail/" + roomId, chat);
         }
+    }
+
+    @GetMapping("/chat/userlist")
+    @ResponseBody
+    public List<String> userList(String roomId) {
+
+        return chatService.getUserList(roomId);
+    }
+
+    @GetMapping("/chat/duplicateName")
+    @ResponseBody
+    public String isDuplicateName(@RequestParam("roomId") String roomId, @RequestParam("username") String username) {
+        return chatService.isDuplicateName(roomId, username);
     }
 }
--- src/main/java/kugods/wonder/app/chat/controller/ChatRoomController.java
+++ src/main/java/kugods/wonder/app/chat/controller/ChatRoomController.java
@@ -1,6 +1,6 @@
 package kugods.wonder.app.chat.controller;
 
-import kugods.wonder.app.chat.domain.ChatRoom;
+import kugods.wonder.app.chat.dto.ChatRoom;
 import kugods.wonder.app.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,35 +8,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Slf4j
 @Controller
+@Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/chatroom")
 public class ChatRoomController {
 
     private final ChatService chatService;
 
-    @GetMapping("/")
-    public String goChatRoom(Model model){
+    @GetMapping
+    public String goChatRoom(Model model) {
         model.addAttribute("list", chatService.findAllRoom());
         log.info("SHOW ALL ChatList {}", chatService.findAllRoom());
+
         return "roomlist";
     }
 
-    @PostMapping("/chat/createroom")
+    @PostMapping
     public String createRoom(@RequestParam String name, RedirectAttributes rttr) {
         ChatRoom room = chatService.createChatRoom(name);
         log.info("CREATE Chat Room {}", room);
         rttr.addFlashAttribute("roomName", room);
-        return "redirect:/";
+
+        return "redirect:/chatroom";
     }
 
-    @GetMapping("/chat/room")
-    public String roomDetail(Model model, String roomId){
+    @GetMapping("/detail")
+    public String roomDetail(Model model, String roomId) {
         log.info("roomId {}", roomId);
         model.addAttribute("room", chatService.findRoomById(roomId));
+
         return "chatroom";
     }
 }
