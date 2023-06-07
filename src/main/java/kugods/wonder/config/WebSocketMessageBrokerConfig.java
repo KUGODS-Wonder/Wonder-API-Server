d27fb444cd59be9061341606c558d992e2269d13
great-park <cksgh1735@gmail.com>
2023-06-07 13:04:00 +0900

    refactor #46: using RabbitMQ Message Broker, not SimpleBroker

--- src/main/java/kugods/wonder/config/WebSocketMessageBrokerConfig.java
+++ src/main/java/kugods/wonder/config/WebSocketMessageBrokerConfig.java
@@ -2,6 +2,7 @@ package kugods.wonder.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +19,9 @@ public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfi
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.setPathMatcher(new AntPathMatcher("."));
+
         registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
     }
 }
