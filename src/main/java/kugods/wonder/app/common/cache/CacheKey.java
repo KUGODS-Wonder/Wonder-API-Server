dddc2a45a3c394f21cfd6960c8de3bb8d686f2e1
great-park <cksgh1735@gmail.com>
2023-09-10 23:19:33 +0900

    refactor #50: cachekey 리펙토링

--- src/main/java/kugods/wonder/app/common/cache/CacheKey.java
+++ src/main/java/kugods/wonder/app/common/cache/CacheKey.java
@@ -3,11 +3,18 @@ package kugods.wonder.app.common.cache;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import static kugods.wonder.app.common.cache.TTLKey.*;
+
 @RequiredArgsConstructor
 public enum CacheKey {
-    LONG("long", 60 * 60),
-    SHORT("short", 60 * 1),
-    MEDIUM("medium", 60 * 5);
+
+    WALKS_BY_USER_LOCATION("walks_by_user_location", ONE_DAY.getExpiryTimeSec()),
+    WALK_INFO_BY_USER_LOCATION("walk_info_by_user_location", ONE_DAY.getExpiryTimeSec()),
+    TAG_MAP_OF_WALK_LIST("tag_map_of_walk_list", ONE_DAY.getExpiryTimeSec()),
+    TAG_INFO_OF_WALK("tag_info_of_walk", ONE_MONTH.getExpiryTimeSec()),
+    INTERMEDIATE_LOCATION_OF_WALK("intermediate_location_of_walk", ONE_MONTH.getExpiryTimeSec()),
+    MY_RESERVATIONS("my_reservations", ONE_MIN.getExpiryTimeSec())
+    ;
 
     private final String key;
     private final int expiryTimeSec;
